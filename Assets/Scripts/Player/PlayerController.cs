328c99645ca6feafaef48fd069b1e248da68720e
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-05-01 19:01:58 -0300

    FEAT: Adicionei as moveimentação do personagem.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private Rigidbody2D rigidBody2D;
+
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private Vector2 moveDirection;
+    private void FixedUpdate() {
+        rigidBody2D.linearVelocity = moveDirection * moveSpeed;
+    }
+    public void Move(InputAction.CallbackContext context) {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+}
