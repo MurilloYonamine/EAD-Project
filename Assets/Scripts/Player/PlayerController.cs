9cbaa13450975bfd42b0bd96e049c95e88d64230
Felipe <167246574+DIABOEVAeADAO@users.noreply.github.com>
2025-05-11 22:35:22 -0300

    w

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidBody2D;
+    [SerializeField] private float moveSpeed = 10f;
+    private Vector2 currentVelocity;
+    [SerializeField] private Vector2 moveDirection;
+
+    [SerializeField] private LayerMask interactiveLayer;
+    private bool canInteract = false;
+
+    [SerializeField] private TextMeshProUGUI interactionCounterText;
+    private int interactionCount = 0;
+    private string interactionText;
+
+    private void FixedUpdate()
+    {
+        Vector2 targetVelocity = moveDirection * moveSpeed;
+        rigidBody2D.linearVelocity = Vector2.SmoothDamp(rigidBody2D.linearVelocity, targetVelocity, ref currentVelocity, 0.05f);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.started && canInteract)
+        {
+            interactionCount++;
+            interactionCounterText.text = $"Interagiu {interactionCount} vezes!";
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision2D)
+    {
+        if (((1 << collision2D.gameObject.layer) & interactiveLayer) != 0)
+        {
+            Debug.Log("Entrou na range!");
+            canInteract = !canInteract;
+            interactionCounterText.color = Color.green;
+            interactionText = $"Entrou na range!";
+            interactionCounterText.text = interactionText;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision2D)
+    {
+        Debug.Log("Saiu na range!");
+        canInteract = !canInteract;
+        interactionCounterText.color = Color.red;
+        interactionText = $"Está longe da range!"; ;
+        interactionCounterText.text = interactionText;
+    }
+}
